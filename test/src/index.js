732a01dd5b77fc2145d4ec3e9879a42e05f58da6
uehwan85 <uehwan@gmail.com>
2018-05-14 10:57:10 +0900

    optimize

--- test/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../src/components/App';
-import registerServiceWorker from '../src/registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();

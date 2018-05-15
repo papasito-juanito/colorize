f359de9130a0f4aee7d7e5b25d394b1f006cd472
uehwan85 <uehwan@gmail.com>
2018-05-15 15:51:07 +0900

    revise reviews

---
+++ server/db/Reviews/MyReview.js
@@ -0,0 +1,14 @@
+const query = `
+SELECT r.id id, b.brandName, i.itemName item, r.reviewPhoto photo, r.reviewRating rating, r.reviewMessage message, r.reviewTime writeAt, rl.likes
+FROM brands b, items i, users u, reviews r, (SELECT COUNT(reviews_id) likes FROM reviewLikes, reviews, items WHERE items.id = reviews.items_id AND reviews.id = reviewLikes.reviews_id) rl
+WHERE r.reviewToggle='true' AND b.id=i.brands_id AND u.id=r.users_id AND i.id=r.items_id AND u.id='3';
+`
+module.exports = query;
+
+/* result
++----+--------------+-----------+--------+--------+--------------+---------------------+-------+
+| id | brandName    | item      | photo  | rating | message      | writeAt             | likes |
++----+--------------+-----------+--------+--------+--------------+---------------------+-------+
+|  1 | 입생로랑       | 뺴뺴로      | 포토    |      5 | 별로좋음       | 2018-05-15 15:24:30 |     0 |
++----+--------------+-----------+--------+--------+--------------+---------------------+-------+
+*/
--- server/db/Reviews/index.js
+++ server/db/Reviews/index.js
@@ -3,9 +3,10 @@ const UserInfo = require('./UserInfo');
 const Write = require('./Write');
 const Modify = require('./Modify');
 const Delete = require('./Delete');
-const Review = require('./Review');
+const ItemReview = require('./ItemReview');
+const MyReview = require('./MyReview');
 const Delete = require('./Delete');
 
-const Reviews = { UserInfo, Write, Modify, Delete, Review, Delete };
+const Reviews = { UserInfo, Write, Modify, Delete, ItemReview, MyReview, Delete };
 
 module.exports = Reviews;

14bafd4da6dc2f6b4e4c5bbd610ce224f7246e4d
uehwan85 <uehwan@gmail.com>
2018-05-21 11:38:49 +0900

    fix /api/item/rate

--- server/db/items/rate.js
+++ server/db/items/rate.js
@@ -1,22 +1,22 @@
 const query = `
-SELECT * FROM
-  (SELECT ic.id color_id, IFNULL(AVG(rt.reviewRating),0) avg, 
-    COUNT(r1.id) s, COUNT(r2.id) ss, COUNT(r3.id) sss, COUNT(r4.id) ssss, 
-    COUNT(r5.id) sssss, COUNT(rt.id) total
-  FROM itemColors ic
-  LEFT JOIN reviews r1
-  ON ic.id=r1.itemColors_id AND r1.reviewRating='1'
-  LEFT JOIN reviews r2
-  ON ic.id=r2.itemColors_id AND r2.reviewRating='2'
-  LEFT JOIN reviews r3
-  ON ic.id=r3.itemColors_id AND r3.reviewRating='3'
-  LEFT JOIN reviews r4
-  ON ic.id=r4.itemColors_id AND r4.reviewRating='4'
-  LEFT JOIN reviews r5
-  ON ic.id=r5.itemColors_id AND r5.reviewRating='5'
-  LEFT JOIN reviews rt
-  ON ic.id=rt.itemColors_id
-  GROUP BY ic.id) rate
-WHERE rate.color_id=?;
+SELECT ic.id color_id, 
+  COUNT(r1.reviewRating) s,
+  COUNT(r2.reviewRating) ss,
+  COUNT(r3.reviewRating) sss,
+  COUNT(r4.reviewRating) ssss,
+  COUNT(r5.reviewRating) sssss
+FROM itemColors ic
+LEFT JOIN reviews r1
+ON ic.id=r1.itemColors_id AND r1.reviewRating='1'
+LEFT JOIN reviews r2
+ON ic.id=r2.itemColors_id AND r2.reviewRating='2'
+LEFT JOIN reviews r3
+ON ic.id=r3.itemColors_id AND r3.reviewRating='3'
+LEFT JOIN reviews r4
+ON ic.id=r4.itemColors_id AND r4.reviewRating='4'
+LEFT JOIN reviews r5
+ON ic.id=r5.itemColors_id AND r5.reviewRating='5'
+WHERE ic.id=?
+GROUP BY color_id;
 `
 module.exports = query;

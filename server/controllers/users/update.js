e94509ba26bdb228af3c243c3e9103d6cb5916ef
uehwan85 <uehwan@gmail.com>
2018-05-23 22:22:06 +0900

    check /api/user/update

--- server/controllers/users/update.js
+++ server/controllers/users/update.js
@@ -3,22 +3,26 @@ const model = require('../../models/users/update');
 const middleware = require('../../middlewares/hashPassword');
 const db = require('../../db');
 
-module.exports = function(req, res) {
+module.exports = (req, res) => {
 
-  middleware(req.body.userPassword).then(function(hash) {
+  middleware(req.body.userPassword).then(hash => {
     
-    const userPassword=hash;
-    const { userMail, userName, toneName, birthDate, gender } = req.body;
-    const params = [ userMail, userPassword, userName, toneName, birthDate, gender ];
+    const userPassword = hash;
+    const {user_id, userMail, userName, toneName, birthDate, gender} = req.body;
+    const params = [userMail, userPassword, userName, toneName, birthDate, gender, user_id];
 
-    db.query(`SELECT id FROM users WHERE userMail="${userMail}";`, function(err, rows) {
-      if (rows.length !== 0) { res.status(404).end('[server    ] userMail exists...') }
+    db.query(`SELECT userToggle FROM users WHERE id=
+      ${req.body.user_id};`, (err, rows) => {
+      if (err) throw err;
+      else if (!rows.length) res.status(401).send(
+        {'result': false, 'message': 'invalid user_id'})
       else {
-        model(params, function(err, rows) {
-          if (err) { throw err }
-          else { res.status(200).end('[server    ] signup success...') }
+        model(params, (err, rows) => {
+          if (err) throw err;
+          else res.status(200).send(
+            {'result': true, 'message': ` updated`});
         })
       }
-    }) 
+    })
   })
 };
--- server/db/users/update.js
+++ server/db/users/update.js
@@ -1,1 +1,3 @@
-module.exports = ``;
+module.exports = `UPDATE users SET userMail=?, userPassword=?, userName=?, 
+  tones_id=(SELECT id FROM tones WHERE toneName=?), birthDate=?, gender=? 
+  WHERE id=?`;

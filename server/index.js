6bdd55ea62dde6fed1a4e4d6f9687cb23218692c
uehwan85 <uehwan@gmail.com>
2018-05-24 10:00:43 +0900

    before pull

--- server/index.js
+++ server/index.js
@@ -15,13 +15,13 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(session({ secret: secret, resave: false, saveUninitialized: true }));
+app.use(session({secret: secret, resave: false, saveUninitialized: true, cookie: {secure: true}}));
 app.use('/api', router);
 app.use('/', express.static(path.join(__dirname, './../client/build')));
 
-app.get('/', (req, res) => { sess = req.session });
-app.get('*', (req, res) => { res.sendFile(path.resolve(__dirname, './../client/build/index.html')) });
+app.get('/', (req, res) => {sess = req.session});
+app.get('*', (req, res) => {res.sendFile(path.resolve(__dirname, './../client/build/index.html'))});
 
-app.listen(port, () => { console.log(`[server    ] opening express server on port ${port}...`) });
+app.listen(port, () => { console.log(`[server    ] opening express server on port ${port}...`)});
 
 module.exports = app;

18d8f21c0e808f4212a55e95775387bb29b87bdd
uehwan85 <uehwan@gmail.com>
2018-05-24 21:50:04 +0900

    fix /api/item/get/rate

--- server/db/items/get.js
+++ server/db/items/get.js
@@ -23,42 +23,12 @@ WHERE i.itemToggle='true' AND ic.colorToggle='true' AND
   ic.id=rate.color_id AND ic.id IN (?);
   `,
   rate: `
-SELECT avg.id id,avg.avg avg,avg.total total,star.s s,star.ss ss,star.sss sss,
-  star.ssss ssss,star.sssss sssss
-FROM 
-  (SELECT ic.id id, IFNULL(AVG(r.reviewRating),0) avg,COUNT(r.reviewRating) total
-  FROM itemColors ic
-  LEFT JOIN reviews r
-  ON ic.id=r.itemColors_id
-  GROUP BY ic.id) avg,
-  (SELECT ic.id id, 
-    COUNT(r1.reviewRating) s,
-    COUNT(r2.reviewRating) ss,
-    COUNT(r3.reviewRating) sss,
-    COUNT(r4.reviewRating) ssss,
-    COUNT(r5.reviewRating) sssss
-  FROM itemColors ic
-  LEFT JOIN reviews r1
-  ON ic.id=r1.itemColors_id AND r1.reviewRating='1'
-  LEFT JOIN reviews r2
-  ON ic.id=r2.itemColors_id AND r2.reviewRating='2'
-  LEFT JOIN reviews r3
-  ON ic.id=r3.itemColors_id AND r3.reviewRating='3'
-  LEFT JOIN reviews r4
-  ON ic.id=r4.itemColors_id AND r4.reviewRating='4'
-  LEFT JOIN reviews r5
-  ON ic.id=r5.itemColors_id AND r5.reviewRating='5'
-  GROUP BY ic.id) star
-WHERE avg.id=star.id AND avg.id=?;
-
-
-
-SELECT AVG(reviewRating) avg, COUNT(id) total, 
-  COUNT(IF(reviewRating="1",0,1)) s,
-  COUNT(IF(reviewRating="2",0,1)) ss,
-  COUNT(IF(reviewRating="3",0,1)) sss,
-  COUNT(IF(reviewRating="4",0,1)) ssss,
-  COUNT(IF(reviewRating="5",0,1)) sssss
+SELECT AVG(reviewRating) avg, COUNT(id) total,
+  COUNT(CASE WHEN reviewRating='1' THEN 1 END) s,
+  COUNT(CASE WHEN reviewRating='2' THEN 1 END) ss,
+  COUNT(CASE WHEN reviewRating='3' THEN 1 END) sss,
+  COUNT(CASE WHEN reviewRating='4' THEN 1 END) ssss,
+  COUNT(CASE WHEN reviewRating='5' THEN 1 END) sssss
 FROM reviews
 WHERE itemColors_id=2;
   `

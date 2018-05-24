75897b7d0341cc33c5254322080fc8d38e92ec36
uehwan85 <uehwan@gmail.com>
2018-05-24 15:09:00 +0900

    add login check

--- server/routes/users/get.js
+++ server/routes/users/get.js
@@ -6,5 +6,6 @@ const controller = require('../../controllers/users/get');
 
 router.get('/login', controller.login);
 router.get('/logout', controller.logout);
+router.get('/check', controller.check);
 
 module.exports = router;

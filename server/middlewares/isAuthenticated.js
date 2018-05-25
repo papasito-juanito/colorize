a2062bb2613610920b3a9b09484b96ebfa6f1ca1
uehwan85 <uehwan@gmail.com>
2018-05-25 14:07:48 +0900

    change auth session to jwt

--- server/middlewares/isAuthenticated.js
+++ server/middlewares/isAuthenticated.js
@@ -1,3 +1,38 @@
-module.exports = () => {
+// Global import
+const jwt = require('jsonwebtoken');
 
+module.exports = (req, res, next) => {
+  var token = req.headers['x-access-token'] || req.query.token;
+	token = token.replace(/(\s*)/g, ""); // 공백제거;
+	if ( !token ) {
+		return res.status(403).json({
+			success : false,
+			message : 'Login first'
+		});
+	}
+
+	const promise = new Promise( (resolve, reject) => {
+		jwt.verify(
+			token,
+			req.app.get('jwt-secret'),
+			(err, decodeed) => {
+				if ( err ) reject(err);
+				resolve(decodeed);
+			}
+		);
+	});
+
+	const onError = (error) => {
+		res.status(403).json({
+			success : false,
+			message : error.message
+		});
+	};
+
+	promise
+	.then( (decodeed) => {
+		req.decodeed = decodeed; 
+		next(); 
+	})
+	.catch(onError);
 }

bd37a26d49626d3f0638f5f956e47359efd4b8e6
uehwan85 <uehwan@gmail.com>
2018-05-25 17:12:15 +0900

    fix /api/user/post/login

--- server/controllers/reviews/post.js
+++ server/controllers/reviews/post.js
@@ -4,26 +4,10 @@ const jwt = require('jsonwebtoken');
 // Local import
 const model = require('../../models/reviews/post');
 
-// module.exports = (req, res) => {
-//   const {color_id, reviewPhoto, reviewRating, user_id, reviewMessage} = req.body;
-//   const params = [color_id, reviewPhoto, reviewRating, user_id, reviewMessage];
-//   model(params, (err, rows) => {
-//     if (err) throw err;
-//     else res.send({
-//       login: req.session.userMail ? true : false,
-//       review_id: req.body.review_id,
-//       post: true
-//     });
-//   })
-// };
-
 module.exports = {
   message: (req, res) => {
-    
-    // console.log('req.headers: ', req.headers);
-    // console.log('req.headers.token: ',req.headers.token);
+
     const userMail = jwt.verify(req.headers.token, 'jwt-secret').userMail;
-    // console.log('decoded: ', decoded);
 
     const {color_id, reviewPhoto, reviewRating, reviewMessage} = req.body;
     const params = [color_id, reviewPhoto, reviewRating, userMail, reviewMessage];

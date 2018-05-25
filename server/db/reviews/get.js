cebb5410b711376c4e7e9ab14b4555b04560f999
uehwan85 <uehwan@gmail.com>
2018-05-25 18:51:23 +0900

    prepare

--- server/db/reviews/get.js
+++ server/db/reviews/get.js
@@ -47,11 +47,17 @@ SELECT r.id review_id, r.reviewPhoto photo, u.userName name,
   YEAR(NOW())-YEAR(u.birthDate) age, t.toneName tone, r.reviewRating rating,
   r.reviewMessage message, r.reviewTime writeAt, rl.likes likes
 FROM itemColors ic, users u, reviews r, tones t, 
-  (SELECT ri.id review_id, IFNULL(COUNT(rli.id),0) likes, rli.reviewToggle 
+  (SELECT ri.id review_id, IFNULL(COUNT(rli.id),0) likes 
   FROM reviews ri
   LEFT JOIN reviewLikes rli 
   ON ri.id=rli.reviews_id GROUP BY ri.id) rl
 WHERE ic.colorToggle='true' AND u.userToggle='true' AND r.reviewToggle='true' AND
   ic.id=r.itemColors_id AND u.id=r.users_id AND t.id=u.tones_id AND 
   r.id=rl.review_id AND ic.id=?
+ORDER BY likes DESC LIMIT 3;
+
+SELECT ri.id review_id, IFNULL(COUNT(rli.id),0) likes 
+  FROM reviews ri
+  LEFT JOIN reviewLikes rli 
+  ON ri.id=rli.reviews_id GROUP BY ri.id
 `

e4e411923aad0ea0d7bd8d5b7c58c2faf7c10917
uehwan85 <uehwan@gmail.com>
2018-05-25 18:29:38 +0900

    fix /api/user/post/login

--- server/db/reviews/get.js
+++ server/db/reviews/get.js
@@ -42,3 +42,16 @@ WHERE u.userToggle='true' AND i.itemToggle='true' AND r.reviewToggle='true' AND
   i.id=ic.items_id AND b.id=i.brands_id AND u.id=r.users_id AND i.id=r.itemColors_id AND u.id=?;
   `
 };
+`
+SELECT r.id review_id, r.reviewPhoto photo, u.userName name,
+  YEAR(NOW())-YEAR(u.birthDate) age, t.toneName tone, r.reviewRating rating,
+  r.reviewMessage message, r.reviewTime writeAt, rl.likes likes
+FROM itemColors ic, users u, reviews r, tones t, 
+  (SELECT ri.id review_id, IFNULL(COUNT(rli.id),0) likes, rli.reviewToggle 
+  FROM reviews ri
+  LEFT JOIN reviewLikes rli 
+  ON ri.id=rli.reviews_id GROUP BY ri.id) rl
+WHERE ic.colorToggle='true' AND u.userToggle='true' AND r.reviewToggle='true' AND
+  ic.id=r.itemColors_id AND u.id=r.users_id AND t.id=u.tones_id AND 
+  r.id=rl.review_id AND ic.id=?
+`

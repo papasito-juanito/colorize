35cd2b0dccc142ac2ac534d9cd4c975bcf19b33c
uehwan85 <uehwan@gmail.com>
2018-05-26 18:31:45 +0900

    add /api/wishlist/update

--- server/controllers/wishLists/post.js
+++ server/controllers/wishLists/post.js
@@ -2,15 +2,8 @@
 const model = require('../../models/wishLists/post');
 
 module.exports = (req, res) => {
-  const {user_id, color_id} = req.body;  
-  const params = [user_id, color_id];
-
   model(params, (err, rows) => {
     if (err) throw err;
-    else res.send({
-      login: req.session.userMail ? true : false,
-      wish_id: req.body.wish_id,
-      post: true
-    });
+    else res.send(rows);
   })
 };

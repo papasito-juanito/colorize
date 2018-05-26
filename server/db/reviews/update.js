6db94adec628f2ceb26097a6fb33f0886a11b5b1
uehwan85 <uehwan@gmail.com>
2018-05-26 16:26:25 +0900

    add /api/review/update/like

--- server/db/reviews/update.js
+++ server/db/reviews/update.js
@@ -1,3 +1,13 @@
-module.exports = `
+module.exports = {
+  message: `
 UPDATE reviews SET reviewPhoto=?, reviewRating=?, reviewMessage=? WHERE id=?;
-`;
+  `,
+  like: {
+    true: `
+UPDATE reviewLikes SET likeToggle="false" WHERE reviews_id=? AND users_id=?;
+    `,
+    false: `
+UPDATE reviewLikes SET likeToggle="true" WHERE reviews_id=? AND users_id=?;
+    `
+  } 
+}

ff3fbb6a00ab333d7b71809321af17346b7c3c42
uehwan85 <uehwan@gmail.com>
2018-05-31 15:31:50 +0900

    fix middleware

--- server/middlewares/isAuthenticated.js
+++ server/middlewares/isAuthenticated.js
@@ -1,13 +1,29 @@
-// Global import
-const jwt = require('jsonwebtoken');
+// Local import
+const db = require('../db');
 
 module.exports = (req, res, next) => {
-  if (req.headers.token === undefined) res.json({ success: false, message: 'provide token' });
-  else {
-    jwt.verify(req.headers.token, 'jwt-secret', (err, decoded) => {
-      if (err) res.json({ success: false, message: err.message });
-      else next(decoded.userMail);
-    });
-  }
-};
-
+  jwt.verify(req.headers.token, 'jwt-secret', function(err, decoded){
+    if(err){
+      console.log(err);
+      
+      if(err.message='jwt expired'){
+        res.json({
+          success: false,
+          message: 'jwt expired'
+        })
+      } 
+    }
+  })
+  const userMail = jwt.verify(req.headers.token, 'jwt-secret').userMail;
+  console.log('userMail: ', userMail);
+  db.query(`SELECT id FROM users WHERE userMail="${userMail}";`, (err, rows) => {
+    console.log('rows: ',rows);
+    if (!rows.length === 1) {
+      res.json({
+        success: false,
+        message: 'invalid userMail'
+      })
+    }
+    else next();
+  })
+}

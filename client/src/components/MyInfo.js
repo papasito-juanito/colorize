a44d011f7e7ed66228c4375855677b26767e092b
BackYoungJae <byj010101@gmail.com>
2018-06-02 12:22:19 +0900

    merge modify

--- client/src/components/MyInfo.js
+++ client/src/components/MyInfo.js
@@ -427,7 +427,7 @@ class MyInfo extends Component {
             
           
             </Container>
-            : "Loading"
+            : <div style={{width:'1000px', height:'1000px', backgroundColor:'yellow'}}> "Loading"</div>
         )
     }
 }

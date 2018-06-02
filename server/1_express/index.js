63f2bf3b6eccafb75000d51b90a565c9d618c4e5
uehwan85 <uehwan@gmail.com>
2018-06-02 19:02:15 +0900

    fix express

--- server/1_express/index.js
+++ server/1_express/index.js
@@ -1,56 +1,15 @@
-// /* eslint-disable */
-// process.env.NODE_ENV = ( process.env.NODE_ENV && ( process.env.NODE_ENV ).trim().toLowerCase() == 'production' ) ? 'production' : 'development';
-// // Global import
-// const express = require('express');
-// const cors = require('cors');
-// const morgan = require('morgan');
-// const bodyParser = require('body-parser');
-// // const fileUpload = require('express-fileupload');
-// const path = require('path');
-
-// // Local import
-// const config = require('./0_config');
-// const router = require('./2_routes');
-// const { url } = require('../client/src/config');
-
-// const app = express();
-
-// app.use(cors());
-// app.use(morgan('dev'));
-// app.use(bodyParser.json());
-// // app.use(fileUpload());
-// app.use('/api', router);
-// app.use('/', express.static(path.join(__dirname, './../client/build/index.html')));
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './../client/build/index.html'));
-// });
-
-// app.listen(config.express.port, () => {
-//   console.log(`[1_express ] activated on ${url}...`);
-// });
-
-// module.exports = app;
-
-
-
-
-
-/* eslint-disable */
-
-process.env.NODE_ENV = ( process.env.NODE_ENV && ( process.env.NODE_ENV ).trim().toLowerCase() == 'production' ) ? 'production' : 'development';
+process.env.NODE_ENV = (process.env.NODE_ENV && (process.env.NODE_ENV).trim().toLowerCase() == 'production') ? 'production' : 'development';
 // Global import
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const multer = require('multer');
 const path = require('path');
-const { url } = require('../../client/src/config');
 
 // Local import
-const { port, secret } = require('../0_config');
-const router = require('../2_routes');
+const config = require('./../0_config');
+const router = require('./../2_routes');
+const { url } = require('../../client/src/config');
 
 const app = express();
 
@@ -64,8 +23,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './../../client/build/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`[server    ] opening express server on ${url}...`);
+app.listen(config.express.port, () => {
+  console.log(`[1_express ] activated on ${url}...`);
 });
 
 module.exports = app;

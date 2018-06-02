84f9b9bd25ab28396a2bdcea15730f2da4f770af
uehwan85 <uehwan@gmail.com>
2018-06-02 16:22:49 +0900

    fix server

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@
 
 /* eslint-disable */
 
-process.env.NODE_ENV = ( process.env.NODE_ENV && ( process.env.NODE_ENV ).trim().toLowerCase() == 'production' ) ? 'production' : 'development';
+// process.env.NODE_ENV = ( process.env.NODE_ENV && ( process.env.NODE_ENV ).trim().toLowerCase() == 'production' ) ? 'production' : 'development';
 // Global import
 const express = require('express');
 const cors = require('cors');

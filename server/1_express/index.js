1a04c3f905ec6be1b43641a52209c7b55684f3d3
uehwan85 <uehwan@gmail.com>
2018-06-02 15:09:44 +0900

    fix server

--- server/1_express/index.js
+++ server/1_express/index.js
@@ -1,3 +1,4 @@
+process.env.NODE_ENV = ( process.env.NODE_ENV && ( process.env.NODE_ENV ).trim().toLowerCase() == 'production' ) ? 'production' : 'development';
 // Global import
 const express = require('express');
 const cors = require('cors');
@@ -9,6 +10,7 @@ const path = require('path');
 // Local import
 const config = require('../0_config');
 const router = require('../2_routes');
+const { url } = require('../../client/src/config');
 
 const app = express();
 
@@ -17,14 +19,14 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 // app.use(fileUpload());
 app.use('/api', router);
-app.use('/', express.static(path.join(__dirname, './../client/build')));
+app.use('/', express.static(path.join(__dirname, '../../client/build/index.html')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './../client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../client/build/index.html'));
+});
 
 app.listen(config.express.port, () => {
-  console.log(`[1_express ] activated on port ${config.express.port}...`);
+  console.log(`[1_express ] activated on port ${url}...`);
 });
 
 module.exports = app;

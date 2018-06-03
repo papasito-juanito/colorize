5334c831711a8c1a0c1565f10c29bbb012e1e92e
ssam1213 <wonbok1213@gmail.com>
2018-06-03 18:01:28 +0900

    minor change in sort

--- client/src/components/itemList/ItemList.js
+++ client/src/components/itemList/ItemList.js
@@ -43,7 +43,7 @@ const Btn = styled(Link)`
     &:visited {
         color: white;
     }
-    &: active {
+    &: hover {
         text-shadow: 0 0 5px #EB509F, 0 0 10px #EB509F, 0 0 20px #EB509F, 0 0 30px #EB509F, 0 0 40px #EB509F;
     }
 `;

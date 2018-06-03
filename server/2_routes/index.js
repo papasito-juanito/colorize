1f5e4835d169bea76afca1dbe4ed9dd1e4a1ac74
uehwan85 <uehwan@gmail.com>
2018-06-03 15:45:09 +0900

    fix upload

--- server/2_routes/index.js
+++ server/2_routes/index.js
@@ -12,22 +12,34 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 
 // Local import
-const { multerAWS } = require('../0_config');
 
-const s3 = new aws.S3({ multerAWS });
-const upload = multer({
-  storage: multerS3({
-    s3,
-    bucket: 'colorize.io',
-    metadata(req, file, cb) {
-      cb(null, { fieldName: file.fieldname });
-    },
-    key(req, file, cb) {
-      cb(null, Date.now().toString());
-    },
-  }),
-});
+const memorystorage = multer.memoryStorage();
+const upload = multer({ storage: memorystorage });
 
-router.post('/upload', upload.array('photos', 3), (req, res) => {
-  res.send('Successfully uploaded files!');
+router.post('/upload', upload.array('input field name'), (req, res, next) => {
+  req.files.forEach((fileObj, index) => {
+    fileObj.buffer;
+    fileObj.originalname;
+    fileObj.mimetype;
+
+    aws.config.region = 'ap-northeast-2';
+    aws.config.update({
+      accessKeyId: 'AKIAJHAOFQZPM5DDGSQA',
+      secretAccessKey: '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',
+    });
+
+    const s3_params = {
+      Bucket: 'colorize.io',
+      Key: fileObj.originalname,
+      ACL: 'public-read',
+      ContentType: fileObj.mimetype,
+    };
+
+    const s3obj = new aws.S3({ params: s3_params });
+    s3obj.upload({ Body: fileObj.buffer })
+      .on('httpUploadProgress', (evt) => { console.log(evt); })
+      .send((err, data) => {
+        const url = data.Location;
+      });
+  });
 });

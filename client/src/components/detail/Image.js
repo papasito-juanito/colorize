186128fab103e98dadf8355f0949c6fe4e55e795
seuljeanlee <seuljeanlee@gmail.com>
2018-06-04 16:30:27 +0900

    style changes to detail page

---
+++ client/src/components/detail/Image.js
@@ -0,0 +1,71 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+import noWish from '../../assets/emptyHeart.png';
+import Wish from '../../assets/Heart.png';
+import styled from 'styled-components';
+import axios from 'axios';
+import { url } from '../../config';
+
+const Div = styled.div`
+    height : 100%;
+    border: solid red 3px;
+    width: 30vw;
+    position: relative;
+    box-sizing: border-box; 
+    background-color:white;
+    @media (max-width: 768px) {
+      width: 100%;
+			justify-content:center;
+	}
+`
+
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    justify-content: center;
+    display: block;
+    background: pink;
+    @media (max-width: 768px) {
+		justify-content:center;
+	}
+`
+
+const Wishlist = styled.div`
+    width: 2.5vw;
+    height: 2.5vw;
+    position: absolute;
+    left: 1%;
+    top:1%;
+    cursor : pointer;
+`
+const Heart = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    justify-content: center;
+    display: block;
+    background: pink;
+`
+
+class DetailLeft extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+        }
+    }
+
+    render(){
+        
+        return (
+            <Div>
+                <Image src={this.props.data ? this.props.data[0].photo : null} alt={'lip'} />
+                <Wishlist>
+                    <Heart onClick={this.props.changeWish} src={!this.props.data ? null : this.props.data[0].wish === 'true'  ? Wish : noWish} alt={'wishlist'} />  
+                </Wishlist>
+            </Div>
+        )
+    }
+}
+
+export default DetailLeft;
---
+++ client/src/components/detail/ItemDetail.js
@@ -0,0 +1,177 @@
+/* eslint-disable */
+import React, {Component} from 'react';
+import Chart from './Chart';
+import styled from 'styled-components';
+import axios from 'axios';
+import { url } from '../../config';
+import NumberFormat from 'react-number-format';
+import LinesEllipsis from 'react-lines-ellipsis';
+import Modal from 'react-modal';
+
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+const Wrapper = styled.div`
+    height: 100%;
+    width: 50vw;
+    display: flex;
+    background-color:#F4F5F9;
+    border: solid red 3px;
+    flex-direction: column;
+`;
+const Top = styled.div`
+    height: 70%;
+    width: 100%;
+    display: flex;
+    background-color:#F4F5F9;
+    border: solid red 3px;
+    flex-direction: row;
+`;
+const DetailDiv = styled.div`
+  width: 40%;
+  height: 100%;
+  margin: 0 0 0 3%;
+  border : 1px solid #d9dee8;
+  background-color:white;
+  padding: 5% 5% 5% 0;
+  box-sizing:border-box;
+  padding : 3% 0 0 0;
+  text-align: left;
+  font-size: 1rem;
+  color: black;
+  float: top;
+`;
+const Name = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+const Brand = styled.div`
+`;
+const Category = styled.div`
+  font-size: 0.8rem;
+`;
+const ColorName = styled.div`
+  font-weight: bold;
+  display: inline-block;
+`;
+const ColorBox = styled.div`
+  background: #${props => props.color};
+  width: 100%;
+  height: 7px;
+`;
+const VolPrice = styled.div`
+  display: flex;
+`;
+
+
+const ChartDiv = styled.div`
+    width: 60%;
+    height: 100%;
+`;
+const Description = styled.div`
+  width: 100%;
+  height: 30%;
+  font-size : 1.5em;
+`;
+
+class DetailRight extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      like: false,
+      data: '',
+      popupIsOpen: false,
+    };
+    this._openPopup = this._openPopup.bind(this);
+    this._afterOpenPopup = this._afterOpenPopup.bind(this);
+    this._closePopup = this._closePopup.bind(this);
+  }
+
+  componentDidMount() {
+    // axios.get(`${url}/api/item/rate?color_id=${this.props.match.params.id}`)
+    // console.log(this.detail.offsetHeight)
+    axios.get(`${url}/api/item/get/rate?color_id=${this.props.id}`)
+    // .then((response) => {
+    //     console.log(response.data);
+    //   })
+      .then(response => this.setState({
+        data: response.data.rows,
+      }))
+      .catch(err => console.log(err));
+  }
+
+  _openPopup() {
+    this.setState({ popupIsOpen: true });
+  }
+
+  _afterOpenPopup() {
+    this.subtitle.style.color = 'black';
+    this.title.style.color = 'grey';
+
+  }
+
+  _closePopup() {
+    this.setState({ popupIsOpen: false });
+  }
+
+
+  render() {
+    console.log(this.props.data);
+    return (
+      <Wrapper>
+        <Top>
+        <DetailDiv>
+              <Name>{this.props.data ? this.props.data[0].name : null}</Name>
+              <Brand>{this.props.data ? this.props.data[0].brand : null}</Brand>
+              <Category>{this.props.data ? this.props.data[0].category : null}</Category>
+              <ColorName>{this.props.data ? this.props.data[0].color : null}
+                <ColorBox color={this.props.data? this.props.data[0].hex : null}/>
+              </ColorName>
+              <VolPrice>
+              <div>{this.props.data ? this.props.data[0].volume : null} /&nbsp; </div> 
+                <NumberFormat value={this.props.data ? this.props.data[0].price : 0} displayType="text" thousandSeparator suffix="원" />
+              </VolPrice>
+             <div> 
+
+            </div>
+            </DetailDiv>
+        <ChartDiv>
+              <Chart data={this.state.data} />
+        </ChartDiv>
+        </Top>
+        <Description>
+          <LinesEllipsis
+            text={`${this.props.data ? this.props.data[0].description : null}`}
+            maxLine="3"
+            ellipsis={<span style={{ cursor: 'pointer' }}onClick={this._openPopup}> ...더 보기...</span>}
+            trimRight
+            basedOn="words"/>
+        </Description>
+
+        <Modal
+              isOpen={this.state.popupIsOpen}
+              onAfterOpen={this._afterOpenPopup}
+              onRequestClose={this._closePopup}
+              style={customStyles}
+              contentLabel="Description popup"
+                >
+
+              <h2 ref={subtitle => this.subtitle = subtitle}>상세 정보</h2>
+              <div ref={title => this.title = title} style={{ width: '50vh' }}>{this.props.data ? this.props.data[0].description : null}</div>
+              <button style={{ cursor: 'pointer' }} onClick={this._closePopup}>close</button>
+            </Modal>
+
+      </Wrapper>
+    );
+  }
+}
+
+export default DetailRight;

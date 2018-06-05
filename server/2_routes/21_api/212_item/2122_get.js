e7a32408c6e6380199f3bd3b67f02579f0136431
uehwan85 <uehwan@gmail.com>
2018-06-05 10:24:34 +0900

    add api/item/get/search

--- server/2_routes/21_api/212_item/2122_get.js
+++ server/2_routes/21_api/212_item/2122_get.js
@@ -7,5 +7,6 @@ const controller = require('../../../5_controllers/51_api/512_item/5122_get');
 router.get('/list', controller.list);
 router.get('/detail', controller.detail);
 router.get('/rate', controller.rate);
+router.get('/search', controller.search);
 
 module.exports = router;

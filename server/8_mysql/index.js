866457826abfe2487a17ea8654d48f459db76c77
uehwan85 <uehwan@gmail.com>
2018-06-06 21:38:11 +0900

    fix config for deploy

--- server/8_mysql/index.js
+++ server/8_mysql/index.js
@@ -1,2 +1,20 @@
-module.exports = require('mysql')
-  .createConnection(require('../0_config').mysqlRDS);
+// module.exports = require('mysql')
+//   .createConnection(require('../0_config').mysqlRDS);
+
+// Global import
+const mysql = require('mysql');
+
+// Local import
+const { mysqlRDS } = require('../0_config');
+
+const dbConnection = mysql.createConnection(mysqlRDS);
+
+dbConnection.connect((err) => {
+  if (err) {
+    console.log('[8_mysqlRDS] detected error: ', err);
+  } else {
+    console.log('[8_mysqlRDS] activated...');
+  }
+});
+
+module.exports = dbConnection;

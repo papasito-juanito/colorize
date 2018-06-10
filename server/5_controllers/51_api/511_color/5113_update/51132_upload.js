d1c69a6bfd093a193cab14e2a95541d158f1dc16
uehwan85 <uehwan@gmail.com>
2018-06-11 00:10:38 +0900

    change url

--- server/5_controllers/51_api/511_color/5113_update/51132_upload.js
+++ server/5_controllers/51_api/511_color/5113_update/51132_upload.js
@@ -1,22 +1,48 @@
+// Global import
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+
 // Local import
+const { credentials, bucket } = require('../../../../0_config');
 const model = require('../../../../7_models');
-const check = require('../../../../9_query/91_colors/912_get/9120_check');
 const query = require('../../../../9_query/91_colors/913_update/9132_upload');
 
 module.exports = async (req, res) => {
   try {
     if (process.env.NODE_ENV === 'development') {
-      console.log(`[51132_cont] activated update query: ${query}`);
+      console.log('[322_middle] activated multer-s3 to upload');
     }
-    const url = await model(check, req.body.color_id);
-    console.log('url', url);
 
-    // const {
-    //   itemPhoto, itemColor, itemHex, itemDate, color_id,
-    // } = req.body;
-    // const params = [itemPhoto, itemColor, itemHex, itemDate, color_id];
-    // await model(query, params);
-    // res.json({ success: true, message: 'updated' });
+    const s3 = await new aws.S3({ credentials });
+    const upload = await multer({
+      storage: multerS3({
+        s3,
+        bucket: (req.headers.id)
+          ? `${bucket}/${req.originalUrl.split('/')[2]}/${req.headers.id}`
+          : `${bucket}/${req.originalUrl.split('/')[2]}/${req.user_id}`,
+        acl: 'public-read',
+        cacheControl: 'max-age=31536000',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        storageClass: 'REDUCED_REDUNDANCY',
+        metadata(req, file, cb) {
+          cb(null, { fieldName: file.fieldname });
+        },
+        key(req, file, cb) {
+          cb(null, `${Date.now().toString()}_${file.originalname}`);
+        },
+      }),
+    }).single('file');
+
+    upload(req, res, (err) => {
+      if (err) {
+        res.json({ success: false, message: err.message });
+      } else {
+        const params = [req.file.location, req.file.originalname.split('.')[0]];
+        model(query, params);
+        res.json({ success: true, message: 'updated' });
+      }
+    });
   } catch (error) {
     res.json({ success: false, message: error.stack });
   }
